b793ea22b5802d71e46f1409caae4b40565bb5cc
bt <bttestgit@gmail.com>
2021-05-24 17:05:07 +0530

    Broken the homepage into small components and finished the homepage container items

---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "../../pages/homepage/homepage.scss";
+
+import Directory from "../../components/directory-menu/directory-menu";
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
